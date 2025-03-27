d1f3bb839938454763d7ce017ae904a86de3bc40
Matheus <mt.1_@live.com>
2025-03-27 15:16:41 -0300

    Adiciona caracteristicas, latitude e lngitude. Corrige campo telefone

--- df_imoveis_urls/spiders/extrair_info.py
+++ df_imoveis_urls/spiders/extrair_info.py
@@ -17,21 +17,9 @@ class ExtrairInfoSpider(scrapy.Spider):
 
     def parse(self, response):
             
-            telefone_whatsapp = response.css("a.FalarComOAnunciantePeloWhatsapp::attr(data-link)").get() #whatsapp
-            # telefone = re.search(r"phone=(\d+)", telefone_whatsapp).group(1) if telefone_whatsapp else "Não informado" #whatsapp          
-            # telefone_parte1 = response.css('span.teaser a span::text').get().strip()
-            # telefone_parte2 = response.css('a.telefone_anunciante.complete::text').get().strip()
-            # telefone_completo = f"{telefone_parte1}{telefone_parte2}"
-
-            telefone = re.search(r"phone=(\d+)", telefone_whatsapp).group(1) if telefone_whatsapp else ""  # WhatsApp          
-            telefone_parte1 = (response.css('span.teaser a span::text').get() or "").strip()
-            telefone_parte2 = (response.css('a.telefone_anunciante.complete::text').get() or "").strip()
-
-            # Criar o telefone completo apenas se houver algum valor
-            telefone_completo = f"{telefone_parte1}{telefone_parte2}".strip()
-            telefone_completo = telefone_completo if telefone_completo else "Não informado"
-
-
+            telefone_textos = response.css("h2.telefone *::text").getall()
+            telefone_completo = " ".join(telefone_textos)
+            telefone_formatado = re.sub(r"[^0-9()\-\s]", "", telefone_completo).strip()
 
             yield {
                 
@@ -43,17 +31,21 @@ class ExtrairInfoSpider(scrapy.Spider):
                 "garagens": response.css("h6:contains('Garagens:') small::text").get(default="0").strip(),
                 "posicao_sol": response.css(".row h6:contains('Posição do Sol:') small::text").get(default="Não encontrado").strip(),
                 "area_util": response.css("h6:contains('Área Útil:') small::text").get(default="Não encontrado").strip(),
+                "area_total": response.css(".row h6:contains('Área Total:') small::text").get(default="Não encontrado").strip(),
                 "preco": response.css(".display-5.text-warning.precoAntigoSalao::text").get(default="Não encontrado").strip(),
+                "características" : ", ".join(response.css("#detalhe-imovel ul.checkboxes li::text").getall()),
+                "latitude": response.css("script::text").re_first(r"latitude\s*=\s*([-+]?\d*\.\d+)"),
+                "longitude": response.css("script::text").re_first(r"longitude\s*=\s*([-+]?\d*\.\d+)"),
                 
                 #Dados Vendedor
                 "nome_vendedor": response.css("div.col-8.col-md-5 h6::text").get(default="Não informado").strip(),
                 "creci": response.css("div.col-8.col-md-5 small::text").re_first(r"\d+", "Não informado"),
-                "whatsapp": telefone,
-                "Telefone": telefone_completo,
+                "whatsapp": response.css("a.FalarComOAnunciantePeloWhatsapp::attr(data-link)").re_first(r"phone=(\d+)&"),
+                "telefone": telefone_formatado,
+                "publicado_dias:":response.css("h6:contains('Publicado há:') small::text").get(default="Não informado").strip(),
                 
                 #Detalhes
                 "aceita_financiamento": response.css(".row h6:contains('Aceita Financiamento:') small::text").get(default="Não encontrado").strip(),
-                
                 "posicao_imovel": response.css(".row h6:contains('Posição do Imóvel:') small::text").get(default="Não encontrado").strip(),
                 "nome_edificio": response.css(".row h6:contains('Nome do Edifício:') small::text").get(default="Não encontrado").strip(),
                 "ultima_atualizacao": response.css(".row h6:contains('Última Atualização:') small::text").get(default="Não encontrado").strip(),
@@ -62,6 +54,7 @@ class ExtrairInfoSpider(scrapy.Spider):
                 "unidades_andar" : response.css(".row h6:contains('Unidades no Andar:') small::text").get(default="Não econtrado").strip(),
 
                 #Descrição
+                "titulo" : response.css("h6.mobile-h6.mb-0::text").get(),
                 "descricao": [descricao.replace(";", "") for descricao in response.css("p.texto-descricao::text").getall()],
                 "url_origem": response.url
 
